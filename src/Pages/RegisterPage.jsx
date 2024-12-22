561d890baa7dd7058b6635e36de5c64eaab83be2
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2024-12-22 14:35:47 +0400

    Impliment create user with email and password functionality

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -1,20 +1,43 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { AuthContext } from "../Provider/AuthProvider"
 
 const RegisterPage = () => {
+    const {createUser, setUser} = useContext(AuthContext)
+
+    const handelCreateUser = (e) => {
+        e.preventDefault();
+        const form = e.target;
+
+        const name = form.name.value
+        const pothoURL = form.pothoURL.value
+        const email = form.email.value
+        const password = form.password.value
+
+        createUser(email,password)
+        .then(data=>{
+            const user = data.user;
+            setUser(user)
+            console.log(user);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
   return (
     <div className='flex justify-center items-center bg-gray-100 min-h-screen'>
       <div className='bg-white shadow-lg p-8 rounded-lg w-full max-w-md'>
         <h2 className='mb-6 font-bold text-2xl text-center'>
           Create an account
         </h2>
-        <form>
+        <form onSubmit={handelCreateUser}>
           <div className='mb-4'>
             <label className='block text-gray-700'>Name</label>
             <input
               type='text'
               name="name"
               className='mt-2 px-4 py-2 border rounded-md focus:ring-1 focus:ring-blue-600 w-full focus:outline-none'
-              required
+            //   required
             />
           </div>
           <div className='mb-4'>
@@ -23,6 +46,7 @@ const RegisterPage = () => {
               type='text'
               name="pothoURL"
               className='mt-2 px-4 py-2 border rounded-md focus:ring-1 focus:ring-blue-600 w-full focus:outline-none'
+            //   required
             />
           </div>
           <div className='mb-4'>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,23 @@
-import { createContext } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createContext, useState } from "react";
+import auth from "../Firebase/auth.init";
 
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
-    const user = 'jaber'
+    const [user,setUser] = useState(null);
+
+
+    // Create User Using Email And Password
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
     const authInfo = {
-        user
+        user,
+        setUser,
+        createUser,
     }
     return (
         <AuthContext.Provider value={authInfo}>
